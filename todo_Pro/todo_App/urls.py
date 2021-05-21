cd0bebe0be97e0bff30743e03e39499d74bf66f7
general-zah <m.zahjr007@gmail.com>
2021-05-21 22:03:48 +0530

    done

---
+++ todo_Pro/todo_App/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.task_view,name='task_view'),
+    # path('task',views.task,name='task')
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:updateid>/', views.update, name='update'),
+    path('covtask/',views.TaskListView.as_view(),name='covtask'),
+    path('covdetail/<int:pk>/',views.TaskDetailView.as_view(),name='covdetail'),
+    path('covupdate/<int:pk>/', views.TaskUpdateView.as_view(), name='covupdate'),
+    path('covdelete/<int:pk>/', views.TaskDeleteView.as_view(), name='covdelete'),
+]
